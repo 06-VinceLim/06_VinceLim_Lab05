267ff31a764792df4b02fdaf4c894e6f4c2076bf
06-VinceLim <happyvince999@gmail.com>
2022-02-06 17:43:53 +0800

    Unity Update 3
    Made coin pickable and increase score when coins are picked up as well as effects when a coin is picked up.

---
+++ Assets/Scripts/CoinPartical.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinPartical : MonoBehaviour
+{
+
+    public GameObject Peffects;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Instantiate(Peffects, transform.position, transform.rotation);
+        }
+    }
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+
+    public Text scoreUi;
+
+    int score;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Coins")
+        {
+            Debug.Log("Touched coins");
+
+            score += 10;
+
+            scoreUi.text = "Score : " + score;
+
+            Destroy(other.gameObject);
+        }
+
+        if (other.gameObject.tag == "Water")
+        {
+           
+        }
     }
 }
